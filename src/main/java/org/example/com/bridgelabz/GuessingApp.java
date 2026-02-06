e628ef296cd28acae9f076387b154f1d18c67dd0
sumukhamgajendra <sumukha.gajendra@bridgelabz.com>
2026-02-06 21:57:39 +0530

    Coded Configuration setup

--- src/main/java/org/example/com/bridgelabz/GuessingApp.java
+++ src/main/java/org/example/com/bridgelabz/GuessingApp.java
@@ -1,5 +1,11 @@
 package org.example.com.bridgelabz;
 
+/**
+ *
+ * @author Sumukha
+ * @version 1.0
+ */
+
 public class GuessingApp {
     public static void main(String[] args) {
         System.out.println("Welcome to Guessing app");
